15e9c8e106b8189835efbbd76dcb741714917cbc
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-07-10 20:22:59 +1000

    loading save data

--- scripts/save.gd
+++ scripts/save.gd
@@ -1,7 +1,28 @@
 extends Node
 
-func _save_settings():
-	pass
+const SAVE_PATH = "user://"
+const FILE_NAME= "save.txt"
 
-func _load_settings():
-	pass
+
+func _ready() -> void:
+	Globals.loaded_save = _load_file()
+
+
+func _save_file(data) -> void:
+	var create_folder = DirAccess.open("user://")
+	create_folder.make_dir_recursive(SAVE_PATH)
+	ResourceSaver.save(data,_get_save_path())
+	print("Saved in game slot "+ str())
+
+
+func _get_save_path() -> String:
+	return SAVE_PATH + FILE_NAME
+
+
+func _load_file() -> SaveFile:
+	var _path = _get_save_path()
+	if not FileAccess.file_exists(_path): 
+		var new_file = SaveFile.new()
+		return new_file
+	var file : SaveFile = load(_path)
+	return file
--- scripts/save_file.gd
+++ scripts/save_file.gd
@@ -1,7 +1,10 @@
-class_name SaveFile 
+class_name SaveFile
 extends Resource
 
 # You can edit what is stored in the save file
 # Remember to give every variable a default value
 @export var high_score = 100
+@export var captain_name = "John"
+@export var chef_name = "Head Chef"
+@export var sous_chef_name = "Sous-Chef"
 
