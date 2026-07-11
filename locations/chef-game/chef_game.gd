c46badb1b6e96ef92dc30ef8ad5a174f58b30361
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-07-11 17:27:58 +1000

    completed chef game

--- locations/chef-game/chef_game.gd
+++ locations/chef-game/chef_game.gd
@@ -1,12 +1,58 @@
 extends Control
 
-@onready var dialogue_node : Control = get_tree().get_first_node_in_group("dialogue")
+@onready var button1 = %Button
+@onready var button2 = %Button2
+@onready var question = %Question
+@onready var label = %Label
+
+var chosen = ""
+signal choice_made
 
 func _ready() -> void:
-	#Globals.._show_dialogue_line()
+	button1.pressed.connect(Callable(_pressed_button).bind(button1))
+	button2.pressed.connect(Callable(_pressed_button).bind(button2))
 
-	await dialogue_node.finished_dialogue
+func _start():
+	question.hide()
+	Globals.dialogue_played.emit("chef-part1")
+	await Globals.finished_dialogue
+	await _ask_question("What should go in the pot?", "Chocolate", "Onion")
+	Globals.loaded_save.keywords["[ing1]"] = chosen
+	if chosen == "Chocolate":
+		Globals.loaded_save.won_sous_chef = SaveFile.WIN_STATES.WON
+	if chosen == "Onion":
+		Globals.loaded_save.won_chef = SaveFile.WIN_STATES.WON
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	Globals.dialogue_played.emit("chef-part2")
+	await Globals.finished_dialogue
+	await _ask_question("What should go in the pot?", "Banana", "Chicken stock")
+	Globals.loaded_save.keywords["[ing2]"] = chosen
+	if chosen == "Chicken stock":
+		Globals.loaded_save.won_sous_chef = SaveFile.WIN_STATES.WON
+	if chosen == "Banana":
+		Globals.loaded_save.won_chef = SaveFile.WIN_STATES.WON
+
+	Globals.dialogue_played.emit("chef-part3")
+	await Globals.finished_dialogue
+	if Globals.loaded_save.won_chef == SaveFile.WIN_STATES.WON:
+		Globals.dialogue_played.emit("win-chef")
+		await Globals.finished_dialogue
+	if Globals.loaded_save.won_sous_chef == SaveFile.WIN_STATES.WON:
+		Globals.dialogue_played.emit("win-sous-chef")
+		await Globals.finished_dialogue
+
+
+func _pressed_button(choice : Button):
+	chosen = choice.text
+	choice_made.emit()
+
+
+func _ask_question(question_text, opt1, opt2):
+	question.show()
+	label.text = question_text
+	button1.text = opt1
+	button2.text = opt2
+	await choice_made
+	question.hide()
+	return
+
