75d5d0c86b51051afcc8870193baa6d5bc8faf3b
thespidaecorsair <julia.garcia.guio@gmail.com>
2026-07-12 00:29:19 +1000

    fixed target spawning

--- cannonerminigame/cannon_target.gd
+++ cannonerminigame/cannon_target.gd
@@ -6,6 +6,8 @@ func _ready():
 	print("created")
 	velocity.y = 200
 	
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
 func check_if_despawn(pos):
 	if pos.y == 2000:
 		self.queue_free()
--- cannonerminigame/cannongame.gd
+++ cannonerminigame/cannongame.gd
@@ -13,9 +13,3 @@ func spawn_new():
 	var targetNew = target.instantiate()
 	targetNew.position.x = randf_range(0, 700)
 	add_child(targetNew)
-
-func _process(_delta):
-	timer -= _delta
-	while timer <= 0:
-		spawn_new()
-		timer += timer.wait_time
