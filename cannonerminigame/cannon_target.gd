0654fc85cbcbf1a680cd286f595ae7839c4d7ea2
thespidaecorsair <julia.garcia.guio@gmail.com>
2026-07-11 23:44:53 +1000

    bug fixes to cannon game

--- cannonerminigame/cannon_target.gd
+++ cannonerminigame/cannon_target.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+var speed = 100
+
 func _process(_delta):
-	position.y += 700 * _delta
+	position.y += 700 + speed * _delta
 	check_if_despawn(position.y)
 	
 func check_if_despawn(pos):
-	if pos.y >= 700:
+	if pos.y == 2000:
 		self.queue_free()
--- cannonerminigame/cannongame.gd
+++ cannonerminigame/cannongame.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 func _ready():
 	var timer = Timer.new()
 	timer.wait_time = 1
@@ -9,5 +10,6 @@ func _ready():
 
 func _on_timeout():
 	var target = preload("res://cannonerminigame/cannon_target.tscn").instantiate()
+	target.position.x = 100
+	print("summoned target")
 	target.position.y = 700
-	target.position.x = range(941, 767)
